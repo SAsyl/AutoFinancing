622db52c5377bc8e127e90d3c0bb6350c1e15750
Shakhputov Assylkhan <asylkhan.sh@mail.ru>
2025-06-11 22:52:33 +0500

    Accounting for /income by category, Weather forecast & Logging

--- bot.py
+++ bot.py
@@ -8,12 +8,41 @@ import io
 
 from dotenv import load_dotenv
 import os
+import logging
+
+import requests
 
 load_dotenv()
 
 BOT_API_TOKEN = os.getenv("AutoFinancingBot_API_key")
+
+WEATHER_API_TOKEN = os.getenv("Weather_API_key")
+
 db_expenses = os.getenv("db_expenses")
+db_incomes = os.getenv("db_incomes")
 db_users = os.getenv("db_users")
+
+income_categories = {"Bills & Charges": ["Bonus Back", "Deposit", "Refund"],
+                     "Correction": ["Correction"],
+                     "Gifts": ["Gifts"],
+                     "Salary": ["Monthly Share", "Salary"]}
+
+# Хранилище состояний и данных пользователей
+user_state = {}
+# Возможные этапы
+STEPS_INCOME = ['category', 'subcategory', 'amount_description_date']
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("logs/bot.log", encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+)
 
 def manual_insert():
     user_id = input("User ID: ")
@@ -29,18 +58,20 @@ def manual_insert():
 
 def start_bot():
     bot = telebot.TeleBot(BOT_API_TOKEN)
-    print("Bot started!")
+    # print("Bot started!")
 
     @bot.message_handler(commands=['start'])
     def send_welcome(message):
         user_name = message.from_user.first_name
         user_surname = message.from_user.last_name
+        user_id = message.from_user.id
         bot.send_message(message.chat.id, f"👋 Приветствую {user_name} {user_surname}! Я бот для учёта расходов.")
 
         register_user_if_needed(message)
         bot.send_message(message.chat.id, f"Для того, чтобы узнать о командах бота, напишите /help")
 
-        print("Handed: /start")
+        logging.info(f"/start от {user_name} {user_surname}")
+        # print("Handed: /start")
 
     def register_user_if_needed(message):
         user_id = int(message.from_user.id)
@@ -64,11 +95,13 @@ def start_bot():
 
             # bot.send_message(message.chat.id,
             #                  f"✅ Пользователь {user_fullname} зарегистрирован!")
-            print("✅ User registered:", user_fullname, user_id)
+            logging.info(f"✅ User registered: {user_fullname}, {user_id}")
+            # print("✅ User registered:", user_fullname, user_id)
         else:
             # bot.send_message(message.chat.id,
             #                  f"👤 Пользователь уже существует: {user_fullname} ({user_id})")
-            print("👤 User already exists:", user_fullname, user_id)
+            logging.info(f" User already exists: {user_fullname}, {user_id}")
+            # print("👤 User already exists:", user_fullname, user_id)
 
     @bot.message_handler(commands=['graph'])
     def send_graph(message):
@@ -81,7 +114,9 @@ def start_bot():
 
         option_msg = bot.send_message(message.chat.id, "Выберите опцию из списка:", reply_markup=markup)
         bot.register_next_step_handler(option_msg, process_graph)
-        print("Handed: /graph")
+
+        logging.info(f"/graph от {message.from_user.first_name} {message.from_user.last_name}")
+        # print("Handed: /graph")
 
     def process_graph(message):
         user_id = message.from_user.id
@@ -151,16 +186,18 @@ def start_bot():
 
             bot.send_photo(chat_id, buffer)
         else:
-            print("Handed: /graph")
+            logging.warning(f"Unexpected /graph option: {message.text}")
+            # print("Handed: /graph")
 
     @bot.message_handler(commands=['help'])
     def send_help(message):
-        bot.send_message(message.chat.id, "Команды: /start /expense /graph")
-        print("Handed: /help")
+        bot.send_message(message.chat.id, "Команды: /start /expense /income /graph /current_weather /forecast_weather")
+        logging.info(f"/help от {message.from_user.first_name} {message.from_user.last_name}")
+        # print("Handed: /help")
 
     # Step 1: /add command
     @bot.message_handler(commands=['expense'])
-    def handle_add_command(message):
+    def handle_expense_command(message):
         expenses_msg = bot.send_message(message.chat.id, "Введите расходы в формате:\nКатегория | Подкатегория | Сумма | Краткое описание | Магазин | Дата (если сегодня, то можно пропустить, иначе в формате DD.MM.YYYY (HH:MM:SS))")
         # expenses_msg = bot.send_message(message.chat.id, "Или же выберите категорию из следующего списка:")
         #
@@ -174,7 +211,8 @@ def start_bot():
         # bot.register_next_step_handler(category_msg, process_category)
 
         bot.register_next_step_handler(expenses_msg, process_expense_input)
-        print("Handed: /expense")
+        logging.info(f"/expense от {message.from_user.first_name} {message.from_user.last_name}")
+        # print("Handed: /expense")
 
     # def process_category(message):
     #     category = message.text.strip()
@@ -186,7 +224,8 @@ def start_bot():
         try:
             if message.text.strip() == '/finish':
                 bot.send_message(message.chat.id, "Расходы сохранены!")
-                print("Handed: /finish")
+                logging.info(f"/finish (expense) от {message.from_user.first_name} {message.from_user.last_name}")
+                # print("Handed: /finish")
                 return
 
             parts = message.text.strip().lower().split('|')
@@ -217,22 +256,159 @@ def start_bot():
             conn.close()
 
             # Send success message
-            bot.send_message(message.chat.id, f"✅ Расход сохранён: {category}({subcategory}) in {store} — {amount} тг")
-            print(f"✅ Расход сохранён: {category}({subcategory}) in {store} — {amount} тг")
+            bot.send_message(message.chat.id, f"✅ Расход сохранён: {category} ({subcategory}) in {store} — {amount} тг")
+            logging.info(f"Расход сохранён: {category} ({subcategory}) in {store} — {amount} тг от {message.from_user.first_name} {message.from_user.last_name}")
+            # print(f"✅ Расход сохранён: {category} ({subcategory}) in {store} — {amount} тг")
 
             bot.send_message(message.chat.id, "Продолжить вводить расходы? Если нет, то напишите /finish")
             bot.register_next_step_handler(message, process_expense_input)
         except Exception as e:
+            logging.warning(f"⚠️ Задан неверный формат \expense от {message.from_user.first_name} {message.from_user.last_name}")
             bot.send_message(message.chat.id,
                              "⚠️ Неверный формат. Пожалуйста, введите как: Категория | Подкатегория | Сумма | Краткое описание | Магазин | Дата\nПример: transport | taxi | 1300 | Work -> Home | YangexGo | ")
 
-    try:
-        bot.polling()
-    except KeyboardInterrupt:
-        print("Bot stopped by user.")
+    # Step 1: /add command
+    @bot.message_handler(commands=['income'])
+    def handle_income_command(message):
+        income_data = []
+        logging.info(f"/income от {message.from_user.first_name} {message.from_user.last_name}")
+
+        user_state[message.chat.id] = {'step': 'category'}
+
+        # bot.send_message(message.chat.id,
+        #                                 "Введите доход в формате:\nКатегория | Подкатегория | Сумма | Краткое описание | Дата (если сегодня, то можно пропустить, иначе в формате DD.MM.YYYY (HH:MM:SS))")
+        # income_msg = bot.send_message(message.chat.id, "Выберите категорию из следующего списка:")
+
+        markup_category = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        for category in income_categories.keys():
+            markup_category.add(category)
+
+        bot.send_message(message.chat.id, "Выберите категорию дохода:", reply_markup=markup_category)
+
+    @bot.message_handler(func=lambda message: message.chat.id in user_state)
+    def handle_income_steps(message):
+        chat_id = message.chat.id
+        state = user_state[chat_id]
+        step = state['step']
+
+        if step == 'category':
+            selected_category = message.text.strip()
+            state['category'] = selected_category
+            state['step'] = 'subcategory'
+
+            markup_subcategory = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            for subcategory in income_categories[selected_category]:
+                markup_subcategory.add(subcategory)
+
+            bot.send_message(chat_id, "Выберите подкатегорию дохода:", reply_markup=markup_subcategory)
+        elif step == 'subcategory':
+            selected_subcategory = message.text.strip()
+            state['subcategory'] = selected_subcategory
+            state['step'] = 'amount_description_date'
+
+            bot.send_message(chat_id, f"Введите Сумма | Краткое описание | Дата в формате DD.MM.YYYY (если сегодня, то можно пропустить)")
+        elif step == 'amount_description_date':
+            try:
+                amount, description, date = message.text.split('|')
+
+                amount = float(amount.strip())
+                description = description.strip()
+                if date is None or date.strip() == '':
+                    date = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+
+                chat_id = message.chat.id
+                user_id = message.from_user.id
+                category = state['category']
+                subcategory = state['subcategory']
+
+                # Save to DB
+                conn = sqlite3.connect(db_incomes)
+                cursor = conn.cursor()
+                cursor.execute("""
+                                INSERT INTO incomes (user_id, category, subcategory, amount, description, date)
+                                VALUES (?, ?, ?, ?, ?, ?)
+                            """, (user_id, category, subcategory, amount, description, date))
+                conn.commit()
+                conn.close()
+
+                user_state.pop(chat_id)
+
+                # Send success message
+                bot.send_message(chat_id,
+                                 f"✅ Доход сохранён: {category} ({subcategory}) — {amount} тг")
+                logging.info(
+                    f"✅ Доход сохранён: {category} ({subcategory}) — {amount} тг (income) от {message.from_user.first_name} {message.from_user.last_name}")
+                # print(f"✅ Доход сохранён: {category} ({subcategory}) — {amount} тг")
+            except Exception as e:
+                logging.warning(f"⚠️ Задан неверный формат \income от {message.from_user.first_name} {message.from_user.last_name}")
+                bot.send_message(chat_id,
+                                 "⚠️ Неверный формат. Пожалуйста, введите как: Категория | Подкатегория | Сумма | Краткое описание | Дата\nПример: 20000 | Днюха | 28.02.2003 ")
+
+    @bot.message_handler(commands=['current_weather'])
+    def send_current_weather(message):
+        city = 'Astana'
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_TOKEN}&units=metric&lang=ru"
+
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code == 200:
+            temp = data['main']['temp']
+            weather_desc = data['weather'][0]['description']
+            wind_speed = data['wind']['speed']
+
+            bot.send_message(message.chat.id, f"🌤️ Погода в Астане сейчас:\nТемпература: {temp}°C\nОписание: {weather_desc}\nВетер: {wind_speed} м/с")
+            logging.info(f"/current_weather от {message.from_user.first_name} {message.from_user.last_name}")
+        else:
+            logging.warning(f"⚠️ Ошибка при получении данных о погоде от {message.from_user.first_name} {message.from_user.last_name}")
+
+    @bot.message_handler(commands=['forecast_weather'])
+    def send_forecast_weather(message):
+        city = 'Astana'
+        url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={WEATHER_API_TOKEN}&units=metric&lang=ru"
+
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code == 200:
+            day_count = 0
+            last_date = ""
+
+            for entry in data['list']:
+                dt_txt = entry['dt_txt']  # формат: '2025-06-06 15:00:00'
+                date_str = dt_txt.split(" ")[0]
+                time_str = dt_txt.split(" ")[1][:5]
+
+                # Печатаем только одну запись в день (например, в 12:00)
+                if time_str.startswith("12") and date_str != last_date:
+                    last_date = date_str
+                    day_count += 1
+
+                    temp = entry['main']['temp']
+                    weather = entry['weather'][0]['description']
+                    wind = entry['wind']['speed']
+
+                    date_formatted = datetime.datetime.strptime(date_str, "%Y-%m-%d").strftime("%d.%m.%Y")
+                    bot.send_message(message.chat.id, f"📅 {date_formatted}: {weather.capitalize()}, 🌡{temp}°C, 💨 ветер {wind} м/с")
+                    # print(f"📅 {date_formatted}: {weather.capitalize()}, 🌡 {temp}°C, 💨 ветер {wind} м/с")
+
+                if day_count == 3:
+                    break
+
+            logging.info(f"/forecast_weather от {message.from_user.first_name} {message.from_user.last_name}")
+        else:
+            logging.warning(
+                f"⚠️ Ошибка при получении данных о погоде от {message.from_user.first_name} {message.from_user.last_name}")
+
+    # try:
+    bot.polling()
+    # except KeyboardInterrupt:
+    #     print("Bot stopped by user.")
 
 if __name__ == "__main__":
+    logging.info("The bot has been launched!")
     start_bot()
+    logging.info("Bot stopped!")
 
-    print("Bot stopped!")
+    # print("Bot stopped!")
 
--- db.py
+++ db.py
@@ -6,25 +6,52 @@ import os
 load_dotenv()
 
 db_expenses = os.getenv("db_expenses")
+db_incomes = os.getenv("db_incomes")
 db_users = os.getenv("db_users")
 
-def init_db():
-    conn = sqlite3.connect(db_expenses)
+
+def init_db(db_path):
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS expenses (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER,
-            category TEXT,
-            subcategory TEXT,
-            amount REAL,
-            description TEXT,
-            store TEXT,
-            date TEXT
-        )
-    """)
+
+    if db_path == db_expenses:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS expenses (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                category TEXT,
+                subcategory TEXT,
+                amount REAL,
+                description TEXT,
+                store TEXT,
+                date TEXT
+            )
+        """)
+    elif db_path == db_incomes:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS incomes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                category TEXT,
+                subcategory TEXT,
+                amount REAL,
+                description TEXT,
+                date TEXT
+            )
+        """)
+    elif db_path == db_users:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_fullname TEXT,
+                register_date TEXT
+            )
+        """)
+
     conn.commit()
     conn.close()
+
+    print("DB initialized!")
 
 def add_expense(user_id, category, subcategory, amount, description, store, date=None):
     if date is None or date == '':
@@ -45,20 +72,7 @@ def add_expense(user_id, category, subcategory, amount, description, store, date
     conn.commit()
     conn.close()
 
-def init_user_db():
-    conn = sqlite3.connect(db_users)
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_fullname TEXT,
-            register_date TEXT
-        )
-    """)
-    conn.commit()
-    conn.close()
 
-# if __name__ == "__main__":
-#     init_db()
-#
-#     print("DB initialized!")
+if __name__ == "__main__":
+    # init_db(db_incomes)
+    pass
